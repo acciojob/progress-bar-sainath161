01df5c18cf1585af69e2efca0367efd8848d9d77
Sainath <71361447+sainath161@users.noreply.github.com>
2023-09-23 23:41:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+        const prevButton = document.getElementById('prev');
+        const nextButton = document.getElementById('next');
+
+        let currentIndex = 0;
+
+        nextButton.addEventListener('click', () => {
+            if (currentIndex < circles.length - 1) {
+                currentIndex++;
+                updateProgressBar();
+            }
+        });
+
+        prevButton.addEventListener('click', () => {
+            if (currentIndex > 0) {
+                currentIndex--;
+                updateProgressBar();
+            }
+        });
+
+        function updateProgressBar() {
+            circles.forEach((circle, index) => {
+                if (index === currentIndex) {
+                    circle.classList.add('active');
+                } else {
+                    circle.classList.remove('active');
+                }
+            });
+
+            prevButton.disabled = currentIndex === 0;
+            nextButton.disabled = currentIndex === circles.length - 1;
+        }
